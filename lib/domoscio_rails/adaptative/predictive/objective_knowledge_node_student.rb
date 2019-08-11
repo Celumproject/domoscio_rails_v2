d97b76d81ebbde4e06cae22c0e8b9f09e7752e37
MatthieuMu <matthieu.mugnier@hotmail.fr>
2019-08-11 22:25:11 +0200

    okns: add HTTPCalls::Update to okns

--- lib/domoscio_rails/adaptative/predictive/objective_knowledge_node_student.rb
+++ lib/domoscio_rails/adaptative/predictive/objective_knowledge_node_student.rb
@@ -2,5 +2,6 @@ module DomoscioRails
     # An objective knowledge node student.
     class ObjectiveKnowledgeNodeStudent < Resource
       include DomoscioRails::HTTPCalls::Fetch
+      include DomoscioRails::HTTPCalls::Update
     end
   end
